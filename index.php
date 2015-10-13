5e73224ad709de6137d3ce0d62ef1b7b11542edc
Yuriy Reva <yuriyreva@gmail.com>
2015-10-13 19:49:37 +0300

    init

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </head>
+  <body>
+  <h2>Заголовок</h2>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-6 col-md-push-6"> 
+          <img src="images/1.jpg" class="img-responsive" alt="1">
+        </div>
+        <div class="col-md-6 col-md-pull-6">
+          <img src="images/2.jpg" class="img-responsive" alt="2">
+        </div>
+      </div>
+    </div>
+
+
+  </body>
+</html>
